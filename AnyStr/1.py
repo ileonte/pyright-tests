c1ff0984b055d0e06b98af67b2e431789005383c
Ionuț Leonte <ionut.leonte@gmail.com>
2020-07-14 23:14:17 +0300

    Add AnyStr/1.py

---
+++ AnyStr/1.py
@@ -0,0 +1,34 @@
+from typing import AnyStr, List, Sequence, Union
+
+
+Command = Union[AnyStr, Sequence[AnyStr]]
+
+
+def version1(cmd: Command) -> List[str]:
+    if isinstance(cmd, bytes):
+        return [str(cmd, 'utf-8')]
+    if isinstance(cmd, str):
+        return [cmd]
+
+    ret: List[str] = []
+    for itm in cmd:
+        if isinstance(itm, str):
+            ret.append(itm)
+        else:
+            ret.append(str(itm, 'utf-8'))
+    return ret
+
+
+def version2(cmd: Command) -> List[str]:
+    if isinstance(cmd, bytes):
+        return [str(cmd, 'utf-8')]
+    if isinstance(cmd, str):
+        return [cmd]
+
+    ret: List[str] = []
+    for itm in cmd:
+        if isinstance(itm, str):
+            ret.append(itm)
+        elif isinstance(itm, bytes):
+            ret.append(str(itm, 'utf-8'))
+    return ret
