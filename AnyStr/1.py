0f48f89d191525ca6cb91d03a6fb501008bed146
Ionuț Leonte <ionut.leonte@gmail.com>
2020-07-14 23:38:26 +0300

    AnyStr/1.py - add version3()

--- AnyStr/1.py
+++ AnyStr/1.py
@@ -32,3 +32,20 @@ def version2(cmd: Command) -> List[str]:
         elif isinstance(itm, bytes):
             ret.append(str(itm, 'utf-8'))
     return ret
+
+
+def version3(cmd: Command) -> List[str]:
+    if isinstance(cmd, bytes):
+        return [str(cmd, 'utf-8')]
+    if isinstance(cmd, str):
+        return [cmd]
+
+    ret: List[str] = []
+    for itm in cmd:
+        if isinstance(itm, str):
+            ret.append(itm)
+            continue
+
+        assert(isinstance(itm, bytes))
+        ret.append(str(itm, 'utf-8'))
+    return ret
